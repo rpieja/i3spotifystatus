40819ad3eb0def6c3a9579c013909e04634e5742
little512 <skidmanxdd@gmail.com>
2020-01-19 20:15:23 -0600

    move awk scripts to awk/

--- getInfo.sh
+++ getInfo.sh
@@ -3,11 +3,11 @@ dir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 if [ "$1" = status ]; then
   dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' | tail -n1 | cut -d'"' -f2
 elif [ "$1" = artist ]; then
-  dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk -f ${dir}/spotify_artist.awk | cut -d':' -f2
+  dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk -f ${dir}/awk/spotify_artist.awk | cut -d':' -f2
 elif [ "$1" = song ]; then
-  dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk -f ${dir}/spotify_song.awk | cut -d':' -f2
+  dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk -f ${dir}/awk/spotify_song.awk | cut -d':' -f2
 elif [ "$1" = album ]; then
-  dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk -f ${dir}/spotify_album.awk | cut -d':' -f2
+  dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk -f ${dir}/awk/spotify_album.awk | cut -d':' -f2
 else
   echo "No argument specified to the script who gets info from Spotify. Try using 'status', 'artist' or 'song'."
 fi
